3a82a9926eed3a36027790b5e59639729fab52b1
Mikiyas Eshetu <mikiyasahmed7@gmail.com>
2021-07-19 23:24:12 +0200

    project entity

--- src/main/java/com/miki/pma/controller/Project.java
+++ src/main/java/com/miki/pma/controller/Project.java
@@ -1,5 +1,45 @@
 package com.miki.pma.controller;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Project {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long projectId;
+	public long getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(long projectId) {
+		this.projectId = projectId;
+	}
+	public Project(String name, String stage, String description) {
+		super();
+		this.name = name;
+		this.stage = stage;
+		this.description = description;
+	}
+	private String name;
+	private String stage; //NOTSTARTED FINISHED INPROGRESS
+	private String description;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStage() {
+		return stage;
+	}
+	public void setStage(String stage) {
+		this.stage = stage;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }
