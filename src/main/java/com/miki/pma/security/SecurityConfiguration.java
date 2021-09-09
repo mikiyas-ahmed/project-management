28dda4f6c1e377f17549615699d4b237a7be975a
Mikiyas Eshetu <mikiyasahmed7@gmail.com>
2021-09-09 12:28:05 +0200

    setup a security database

--- src/main/java/com/miki/pma/security/SecurityConfiguration.java
+++ src/main/java/com/miki/pma/security/SecurityConfiguration.java
@@ -1,8 +1,12 @@
 package com.miki.pma.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -12,21 +16,40 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	DataSource datasource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("myuser")
-				.password("pass")
-				.roles("USER")
-		.and()
-		.withUser("miki")
-		.password("pass")
-		.roles("USER");
+		auth.jdbcAuthentication().dataSource(datasource)
+				.withDefaultSchema()
+				.withUser("myuser")
+					.password("pass")
+					.roles("USER")
+				.and()
+				.withUser("miki")
+					.password("pass")
+					.roles("USER")
+				.and()
+				.withUser("admin")
+					.password("pass")
+					.roles("ADMIN");
 	}
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 		
+	}
+	
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+			.antMatchers("/projects/new").hasAnyRole("ADMIN")
+			.antMatchers("/employee/new").hasAnyRole("ADMIN")
+			.antMatchers("/h2_console/**").permitAll()
+			.antMatchers("/").authenticated().and().formLogin();
+		
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
 	}
 
 	
